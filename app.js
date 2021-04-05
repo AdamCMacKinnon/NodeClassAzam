5245b70383cae30e0d378e23b98bc8f0c2ba78bb
Adam MacKinnon <mackinnon.adam@gmail.com>
2021-04-04 22:09:47 -0400

    up to page templates

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
 const express = require('express')
 const app = express();
 
+app.use(express.json()) // THIS IS TO REPLACE BODY PARSER, WHICH IS DEPRECATED
+app.use(express.urlencoded({extended: true}));
 
 //routes
-app.get('/', (req, res) => {
-  res.send('hello express!')
-})
-
-app.get('/movies/comedy', (req, res) => {
-  res.send('Comedy Movies')
-})
-app.get('/movies/romance', (req, res) => {
-  res.send('Romance Movies')
-})
-app.get('/movies/action', (req, res) => {
-  res.send('Action Movies')
-})
-
-
-
-
-// app.get('/movies/year', (req, res) => {
-//   res.send('movies/year')
+// app.get('/', (req, res) => {
+//   res.send('hello express!')
 // })
 
+// QUERY STRING PARAMATERS
+// app.get('/movies', (req, res) => {
+//   res.send('Movies')
+//   console.log(req.query.sort)
+//   console.log(req.query.page)
+// })
+
+
+// USING JSON AS DATA FORMAT
+// app.get('/movies', (req,res) => {
+//   let movies = [
+//     { title: "Lord of the Rings", year: 2001 },
+//     { title: "Spiderman", year: 2018 },
+//     { title: "Black Sheep", year: 1997 }
+//   ]
+
+//   // let movie = { title: "Lord of the Rings", year: 2001 }
+
+//   res.json(movies)
+
+
+// })
+
+//HTTP POST (USED POSTMAN/BODY PARSER)
+
+// app.get('/movies', (req,res) => {
+//   res.send("Movies!")
+// })
+
+// app.post('/movies', (req,res) => {
+//   let title = req.body.title
+//   let year = req.body.year
+//   console.log(title)
+//   console.log(year)
+//   res.send("OK")
+// })
 
 
 
