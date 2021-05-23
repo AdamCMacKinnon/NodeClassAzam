68e86922d4a705a9ce1d502aeb7f11fe555b4ab6
Adam MacKinnon <mackinnon.adam@gmail.com>
2021-05-23 11:33:55 -0400

    added register user function and hashing to sellyourstuff

--- sellyourstuff/app.js
+++ sellyourstuff/app.js
@@ -3,6 +3,13 @@ const express = require('express')
 const app = express()
 const mustacheExpress = require('mustache-express')
 const path = require('path')
+const bodyParser = require('body-parser')
+const models = require('./models')
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 10;
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 const PORT = 3000
 const VIEWS_PATH = path.join(__dirname,'/views')
@@ -14,6 +21,40 @@ app.set('view engine','mustache')
 app.get('/register',(req,res) => {
   res.render('register')
 })
+
+app.post('/register', async (req,res) => {
+
+  let username = req.body.username
+  let password = req.body.password
+
+  let persistedUser = await models.User.findOne({
+    where: {
+      username: username
+    }
+  })
+  if(persistedUser == null){
+
+    bcrypt.hash(password, SALT_ROUNDS, async (error, hash)=>{
+      if(error){
+        res.render('/register', {message: 'There was an error'})
+      } else {
+        let user = models.User.build({
+          username: username,
+          password: hash
+        })
+        let savedUser = await user.save()
+        if(savedUser != null){
+          res.redirect('/login')
+        } else {
+          res.render('/register', {message: "User Already Exists"})
+        }
+      }
+    
+    })
+      }
+    })
+
+ 
 
 app.listen(PORT,() => console.log('Server is running...'))
 
